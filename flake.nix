20dc9a8ecab00dd7d0c7720765b638ff5dfe7bf8
noahpro <noahpro@gmail.com>
2026-04-16 16:11:32 -0400

    flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+        (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_25
+            gnumake
+          ];
+        };
+      });
+    };
+}
